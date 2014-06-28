6773f00c34f0794aafff872e773d35680f1b7e24
kenji tomura <kenji.tomura@ietty.co.jp>
2014-06-28 17:54:07 +0900

    add read button, menu button

--- aruyo/src/com/example/aruyo/MainActivity.java
+++ aruyo/src/com/example/aruyo/MainActivity.java
@@ -4,11 +4,51 @@ import com.example.aruyo.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity{
+	private Button startButton;
+	private static final int MENU_ID_MENU1 = (Menu.FIRST + 1);
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		startButton = (Button)findViewById(R.id.button1);
+		startButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG).show();
+			}
+			
+		});
 	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, MENU_ID_MENU1, Menu.NONE, "あるよ一覧");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean ret = true;
+        switch (item.getItemId()) {
+        default:
+            ret = super.onOptionsItemSelected(item);
+            break;
+        case MENU_ID_MENU1:
+        	Toast.makeText(this, "あるよ一覧", Toast.LENGTH_LONG).show();
+        	ret = true;
+            break;
+        }
+        return ret;
+    }
+    
 }
